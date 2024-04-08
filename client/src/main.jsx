d186e2fe2fa6b6ad08b3c362ed89e13f13db2019
bbiijjeett <bijeetnathbtech@gmail.com>
2024-04-09 01:20:48 +0530

    fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
